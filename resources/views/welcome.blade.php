fd886139fff36193ab63d1019435cdf01a75012d
Rijalai091 <rijalaimanhanif@students.amikom.ac.id>
2024-07-07 23:45:08 +0700

    initail commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title> cat care</title>
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- feather icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <!-- my style -->
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+    <!-- Navbar start-->
+    <nav class="navbar">
+      <a href="#" class="navbar-logo">CAT<span>LOVERS</span>.</a>
+
+      <div class="navbar-nav">
+        <a href="#home">Home</a>
+        <a href="#about">Tentang Kami</a>
+        <a href="#menu">Menu</a>
+        <a href="#contact">Kontak</a>
+      </div>
+
+      <div class="navbar-extra">
+        <a href="#" id="search"><i data-feather="search"></i></a>
+        <a href="#" id="shopping-cart"><i data-feather="shopping-cart"></i></a>
+        <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
+      </div>
+    </nav>
+    <!-- Navbar end-->
+
+    <!-- Hero section start -->
+    <section class="hero" id="home">
+      <main class="content">
+        <h1>Tempat untuk penitipan<span>kucing</span></h1>
+        <p>Segera daftar menjadi member catcare dan dapatkan promo menarik</p>
+        <a href="#" class="cta"> Daftar Sekarang</a>
+      </main>
+    </section>
+    <!-- Hero section end -->
+
+    <!-- about section start -->
+    <section id="about" class="about">
+      <h2><span>Tentang</span> Kami</h2>
+
+      <div class="row">
+        <div class="about-img">
+          <img src="img/cat3.jpg" alt="tentang kami" />
+        </div>
+        <div class="content">
+          <h3>Benefit catecare</h3>
+          <p>
+           1 Pemilik kucing dapat bepergian dengan tenang karena kucing mereka berada di tempat yang aman dan terawat.
+          </p>
+          <p>
+           2 Penjaga yang berpengalaman dan fasilitas yang memadai memastikan kucing mendapatkan perawatan terbaik.
+          </p>
+          <p>
+           3 Berbagai pilihan paket dan layanan tambahan yang dapat disesuaikan dengan kebutuhan kucing dan pemiliknya.
+          </p>
+         
+        </div>
+      </div>
+    </section>
+    <!-- about section end -->
+
+    <!-- menu section start -->
+    <section id="menu" class="menu">
+      <h2><span>pelayanan </span>catcare</h2>
+      <p>
+        lengkapi berbagai macam data diri anda
+      </p>
+
+      <div class="row">
+        <div class="menu-card">
+          <img src="img/gambar25.jpg" alt="espresso" class="menu-card-img" />
+          <h3 class="menu-card-tittle">- Daftar paket -</h3>
+          <p class="menu-card-price"></p>
+        </div>
+        <div class="menu-card">
+          <img src="img/gambar26.jpg" alt="espresso" class="menu-card-img" />
+          <h3 class="menu-card-tittle">- Profilkucing -</h3>
+          <p class="menu-card-price"></p>
+        </div>
+        <div class="menu-card">
+          <img src="img/gambar27.jpg" alt="espresso" class="menu-card-img" />
+          <h3 class="menu-card-tittle">- User Profile-</h3>
+          <p class="menu-card-price"></p>
+        </div>
+        <div class="menu-card">
+          <img src="img/gambar28.png" alt="espresso" class="menu-card-img" />
+          <h3 class="menu-card-tittle">- status  -</h3>
+          <p class="menu-card-price">penitipan</p>
+        </div>
+        
+        
+        
+      
+    </section>
+    <!-- menu section end -->
+
+    <!-- contac section stat -->
+    <section id="contact" class="contact">
+      <h2><span>customer</span> servis</h2>
+      <p>
+        silakan masukan kritik dan saran lewat kontak kami
+      </p>
+
+      <div class="row">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63245.97085555903!2d110.3336450161311!3d-7.803248457448027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a5783a80c7c19%3A0x4b880677539abaa!2sKota%20Yogyakarta%2C%20Daerah%20Istimewa%20Yogyakarta!5e0!3m2!1sid!2sid!4v1686229911055!5m2!1sid!2sid"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          class="map"
+        ></iframe>
+
+        <form action="">
+          <div class="input-group">
+            <i data-feather="user"></i>
+            <input type="text" placeholder="nama" />
+          </div>
+
+          <div class="input-group">
+            <i data-feather="phone"></i>
+            <input type="text" placeholder="no hp" />
+          </div>
+
+          <div class="input-group">
+            <i data-feather="mail"></i>
+            <input type="text" placeholder="keluhan anda" />
+          </div>
+
+          
+          <button type="submit" class="btn">Kirim Pesan</button>
+        </form>
+      </div>
+    </section>
+    <!-- contac section end -->
+
+    <!-- foter start -->
+    <footer>
+      <div class="socials">
+        <a href="https://www.instagram.com/https://www.instagram.com/gaznjaaaaaa_?igsh=Z2h2eGJzaHMwNW4z" target="_blank">
+          <i data-feather="instagram"></i>
+      </a>
+      <a href="https://www.twitter.com/halamanAnda" target="_blank">
+        <i data-feather="twitter"></i>
+    </a>
+        <a href="https://www.facebook.com/https://www.facebook.com/zeza.ikam" target="_blank">
+          <i data-feather="facebook"></i>
+      </a>
+  
+      </div>
+
+      <div class="links">
+        <a href="#home">Home</a>
+        <a href="#about">Tentang Kami</a>
+        <a href="#menu">Menu</a>
+        <a href="#contact">kontak</a>
+      </div>
+
+      <div class="credit">
+        <p>Created by <a href="">Rijal aiman hanif</a>. | &copy; 2023.</p>
+      </div>
+    </footer>
+
+    <!-- foter end -->
+
+    <!-- feather icons -->
+    <script>
+      feather.replace();
+    </script>
+
+    <!-- My Javascript -->
+    <script src="script.js"></script>
+  </body> 
